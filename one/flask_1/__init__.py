9ed0236d3af0d97c554f9ebc688588eb4aca6bd9
mzidan132 <136159336+mzidan132@users.noreply.github.com>
2023-11-27 22:15:18 +0600

    Delete one directory

--- one/flask_1/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask 
-app = Flask(__name__)
-app.debug = True
-@app.route('/')
-def hello_world():
-    return '<h1>Hello, World! I am Flask App 1.</h1> \
-        Please go visit <a href="/flask_app_2">Flask App 2</a>'
--- one/flask_2/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask 
-app = Flask(__name__)
-app.debug = True
-@app.route('/')
-def hello_world():
-    return '<h1>Hello, World! I am Flask App 2.</h1> \
-        Please go visit <a href="/">Flask App 1</a>'
