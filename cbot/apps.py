b7d85192afd895b43b33e3b5c91fd5d25906ffbb
mzidan132 <136159336+mzidan132@users.noreply.github.com>
2023-11-27 22:27:29 +0600

    Add files via upload

--- cbot/apps.py
+++ cbot/apps.py
@@ -9,40 +9,32 @@ from keras.models import load_model
 model = load_model('model.h5')
 import json
 import random
+
 intents = json.loads(open('data.json').read())
 words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
     sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)  
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s: 
-                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
@@ -52,21 +44,39 @@ def predict_class(sentence, model):
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
-    return result
+
+    for intent in list_of_intents:
+        if intent['tag'] == tag:
+            responses = intent['responses']
+
+            if 'response_with_link' in intent:
+                link_response = intent['response_with_link']
+                result = random.choice(responses)
+                return {"text": result, "link_text": link_response, "link": True}
+            else:
+                result = random.choice(responses)
+                return {"text": result, "link": False}
+
+    return {"text": "I'm sorry, I don't understand that.", "link": False}
 
 def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
-    return res
+
+    response_text = res["text"]
+    response_link = res.get("link",'')
+    link_text = res.get("link_text", "")
+
+    if response_link:
+        return f"{response_text} <a href='{link_text}'>{link_text}</a>"
+    else:
+        return response_text
 
 
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 app.static_folder = 'static'
 
 @app.route("/")
@@ -78,6 +88,5 @@ def get_bot_response():
     userText = request.args.get('msg')
     return chatbot_response(userText)
 
-
 if __name__ == "__main__":
-    app.run(host='localhost',port='5001')
+    app.run(host='localhost',port=5001)
