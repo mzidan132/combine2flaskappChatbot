3c21a82e5abc41b0ab49b29e0513fa192575cefa
mzidan132 <136159336+mzidan132@users.noreply.github.com>
2024-01-14 20:54:21 +0600

    apps.py

--- cbot/apps.py
+++ cbot/apps.py
@@ -1,45 +1,52 @@
 import nltk
-nltk.download('popular')
 from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
-
 from keras.models import load_model
-model = load_model('model.h5')
 import json
 import random
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+lemmatizer = WordNetLemmatizer()
+model = load_model('model.h5')
 
 intents = json.loads(open('data.json').read())
-words = pickle.load(open('texts.pkl','rb'))
-classes = pickle.load(open('labels.pkl','rb'))
+words = pickle.load(open('texts.pkl', 'rb'))
+classes = pickle.load(open('labels.pkl', 'rb'))
+
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
 
+
 def bow(sentence, words, show_details=True):
     sentence_words = clean_up_sentence(sentence)
-    bag = [0]*len(words)  
+    bag = [0] * len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
+        for i, w in enumerate(words):
+            if w == s:
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
+                    print("found in bag: %s" % w)
+    return np.array(bag)
+
 
 def predict_class(sentence, model):
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
+
 
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
@@ -49,10 +56,13 @@ def getResponse(ints, intents_json):
         if intent['tag'] == tag:
             responses = intent['responses']
 
-            if 'response_with_link' in intent:
-                link_response = intent['response_with_link']
+            if 'response_with_links' in intent:
+                links_response = intent['response_with_links']
                 result = random.choice(responses)
-                return {"text": result, "link_text": link_response, "link": True}
+                link_texts = []
+                for link in links_response:
+                    link_texts.append(f"{link['message']}<a href='{link['link']}'>{link['text']}</a>")
+                return {"text": result + " ".join(link_texts), "link": True}
             else:
                 result = random.choice(responses)
                 return {"text": result, "link": False}
@@ -68,25 +78,25 @@ def chatbot_response(msg):
     link_text = res.get("link_text", "")
 
     if response_link:
-        return f"{response_text} <a href='{link_text}'>{link_text}</a>"
+        return f"{response_text} <a href='{link_text}'></a>"
     else:
         return response_text
 
 
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-app.static_folder = 'static'
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    return chatbot_response(userText)
+    responses = chatbot_response(userText)
+
+    return json.dumps(responses)
+
 
 if __name__ == "__main__":
-    app.run(host='localhost',port=5001)
+    app.run(host='localhost', port=5001)
+
